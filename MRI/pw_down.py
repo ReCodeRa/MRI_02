397cf0f64d6c932665e207c51a75dddbab116cb1
ReCodeRa <esosacek@centrum.cz>
2023-01-02 22:09:15 +0100

    download_info NOT obtained sync nor async

--- MRI/pw_down.py
+++ MRI/pw_down.py
@@ -5,21 +5,18 @@ key = "DK/H/0241/001"
 
 async def main():
     async with async_playwright() as p:
-        browser = await p.chromium.launch()
+        browser = await p.chromium.launch(headless=False)
         page = await browser.new_page()
-        await page.goto("http://playwright.dev")
-        print(await page.title())
+        await page.goto("https://mri.cts-mrp.eu/portal/details?productnumber=DK/H/0241/001")
+        download_info = page.expect_download()
+        # Perform the action that initiates download
+        await page.get_by_text("Download excel").click()
+        download = download_info.value()
+        # Wait for the download process to complete
+        print(await download.path())
+        # Save downloaded file somewhere
+        download.save_as(f"/{key}/{key}.xlsx")
+        
         await browser.close()
 
 asyncio.run(main())
-
-# Start waiting for the download
-async def down():
-    async with page.expect_download() as download_info:
-    # Perform the action that initiates download
-        await page.get_by_text("Download file").click()
-    download = await download_info.value
-    # Wait for the download process to complete
-    print(await download.path())
-    # Save downloaded file somewhere
-    download.save_as("/path/to/save/download/at.txt")
