459e34ab9e409bd15ca6c7230d43b9937cfc6227
ReCodeRa <esosacek@centrum.cz>
2023-01-02 22:34:15 +0100

    double with

--- MRI/pw_down_sync.py
+++ MRI/pw_down_sync.py
@@ -1,19 +1,19 @@
 from playwright.sync_api import sync_playwright
 
-key = "DK/H/0241/001"
+key = "NL/H/2664/001"
 
 def main():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto("https://mri.cts-mrp.eu/portal/details?productnumber=DK/H/0241/001")
-        download_info = page.expect_download()
-        # Perform the action that initiates download
-        page.get_by_text("Download excel").click()
-    download = download_info.value()
-        # Wait for the download process to complete
-    print(download.path())
-        # Save downloaded file somewhere
+        page.goto("https://mri.cts-mrp.eu/portal/details?productnumber=NL/H/2664/001")
+    with page.expect_download() as download_info:
+        page.get_by_text("Download file").click()
+    download = download_info.value
+    # wait for download to complete
+    path = download.path()
+    print(path)
+    # Save downloaded file somewhere
     download.save_as(f"/{key}/{key}.xlsx")
         
     browser.close()
