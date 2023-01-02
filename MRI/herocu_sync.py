4de900fb35c24033187ab261a0cc04da608e271e
ReCodeRa <esosacek@centrum.cz>
2023-01-03 00:34:43 +0100

    herocu

--- MRI/herocu_sync.py
+++ MRI/herocu_sync.py
@@ -1,15 +1,13 @@
 from playwright.sync_api import sync_playwright
 
-key = "NL/H/2664/001"
-
 def main():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto("https://mri.cts-mrp.eu/portal/details?productnumber=NL/H/2664/001", wait_until='networkidle')
+        page.goto("https://the-internet.herokuapp.com/download", wait_until='networkidle')
 
     with page.expect_download() as download_info:
-        page.get_by_text("Download").click()
+        page.get_by_text("Excel.xlsx").click()
     download = download_info.value
     # wait for download to complete
     path = download.path()
