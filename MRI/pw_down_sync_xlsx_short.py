7b06b2f7c5f4c1467462bf2e78e0d570867a3236
ReCodeRa <esosacek@centrum.cz>
2023-01-03 23:32:06 +0100

    sync.xlsx functional :D

---
+++ MRI/pw_down_sync_xlsx_short.py
@@ -0,0 +1,18 @@
+import os
+from playwright.sync_api import sync_playwright
+
+key = "5163"
+os.makedirs(key, exist_ok=True)
+
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://mri.cts-mrp.eu/portal/details?productnumber=NL/H/5163/001")
+        
+        with page.expect_download() as download_info:
+            page.get_by_text("Download excel").click()
+        download = download_info.value
+        download.save_as(f"{key}/{key}.xlsx")    
+        browser.close()
+main()
