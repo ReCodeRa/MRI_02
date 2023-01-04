94e195b1f9a64bbe7d08a65c635625c915d3e469
ReCodeRa <esosacek@centrum.cz>
2023-01-04 18:38:44 +0100

    sync down all docs

---
+++ MRI/pw_down_sync_all.py
@@ -0,0 +1,21 @@
+import os
+from playwright.sync_api import sync_playwright
+
+key = "2731"
+os.makedirs(f"res/{key}", exist_ok=True)
+
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://mri.cts-mrp.eu/portal/details?productnumber=NL/H/2731/001")
+        
+        with page.expect_download() as download_info:
+            page.get_by_text("Download excel").click()
+        download = download_info.value
+        download.save_as(f"{key}/{key}.xlsx")    
+       
+        # Selector for document download buttons: .mat-button-base.ng-star-inserted
+       
+        browser.close()
+main()
