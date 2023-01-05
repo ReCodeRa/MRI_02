c1ee48e2387e7d808115da898c4756700749355e
ReCodeRa <esosacek@centrum.cz>
2023-01-05 23:32:55 +0100

    Locator use to be studied

--- MRI/pw_down_sync_single_pdf.py
+++ MRI/pw_down_sync_single_pdf.py
@@ -6,9 +6,9 @@ os.makedirs(f"res/{key}", exist_ok=True)
 
 def main():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=False, slow_mo= 5000)
         page = browser.new_page()
-        page.goto("https://mri.cts-mrp.eu/portal/details?productnumber=NL/H/2731/001")
+        page.goto("https://mri.cts-mrp.eu/portal/details?productnumber=NL/H/2731/001", wait_until="networkidle")
         
         # with page.expect_download() as download_info:
         #     page.get_by_text("Download excel").click()
@@ -16,21 +16,23 @@ def main():
         # download.save_as(f"res/{key}/{key}.xlsx")    
        
         # Selector for document download buttons: .mat-button-base.ng-star-inserted
-        elements = page.get_by_role("listitem").get_by_role("button")
+        # STUDY LOCATOR METHODS, esp. "nth" in iterator
+        elements = page.get_by_role("listitem").get_by_role("button").all()
         count = elements.count()
         print(f"Number of detected elements is: {count}")
-        print(elements)
+        for doc in elements:
+        
 
         # for i in range(count):
         #     elements.nth(i).click(modifiers=["Control", "Shift"])
 
         # handles = page.query_selector_all(".documents-list .mat-button-wrapper .mat-icon-no-color")
 
-        # with page.expect_download() as download_info:
-        #     page.query_selector("button[mattooltip='Download']").click()
-        # download = download_info.value
-        # doc_name = download.suggested_filename
-        # download.save_as(f"res/{key}/{doc_name}.pdf")
+            with page.expect_download() as download_info:
+                doc.click()
+            download = download_info.value
+            doc_name = download.suggested_filename
+            download.save_as(f"res/{key}/{doc_name}.pdf")
     
         browser.close()
 main()
