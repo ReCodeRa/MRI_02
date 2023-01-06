39c7306eb1a597f8cbbda14aef53281c6ca2c4fa
ReCodeRa <esosacek@centrum.cz>
2023-01-06 18:33:19 +0100

    pw_down_all_sync FUNTIONAL

--- MRI/pw_down_sync_all.py
+++ MRI/pw_down_sync_all.py
@@ -1,29 +1,42 @@
-import os
 from playwright.sync_api import sync_playwright
+from pathlib import Path
+import os
 
-key = "2731"
+key = "4046"
 os.makedirs(f"res/{key}", exist_ok=True)
+
+path = Path().home() / 'Downloads'
+timeout = 3000                  # 3s timeout
 
 def main():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto("https://mri.cts-mrp.eu/portal/details?productnumber=NL/H/2731/001")
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://mri.cts-mrp.eu/portal/details?productnumber=NL/H/4046/001", timeout=timeout)
+        
         
         with page.expect_download() as download_info:
             page.get_by_text("Download excel").click()
         download = download_info.value
-        download.save_as(f"res/{key}/{key}.xlsx")    
-       
-        # Selector for document download buttons: .mat-button-base.ng-star-inserted
-        handles = page.query_selector_all(".documents-list .mat-button-wrapper .mat-icon-no-color")
+        download.save_as(f"res/{key}/{key}.xlsx") 
+        
+        page.wait_for_selector('mat-card', timeout=timeout) # completely load the page
+        downloads = page.locator('mat-card').locator("button")
+        # texts = await downloads.all_text_contents() # just making sure I got what I thought I got
+        count = downloads.count()
+        print(f'There are {count} buttons in mat-card section.')
 
-        for handle in handles:
-            with page.expect_download() as download_info:
-                handle.query_selector("button[mattooltip='Download']").click()
-            download = download_info.value
+# Starting here is where I can't follow the API
+
+        for i in range(count):
+            print(f"Starting download {i}")
+            with page.expect_download() as download_handler:
+                downloads.nth(i).click(timeout=0)
+            download = download_handler.value
             doc_name = download.suggested_filename
-            download.save_as(f"res/{key}/{doc_name}.pdf")
+            download.save_as(f"res/{key}/{doc_name}")
     
-        browser.close()
+    print("\tDownload acquired...")
+    browser.close()
 main()
