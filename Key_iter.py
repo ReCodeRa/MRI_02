de246a50dafb2b654348189bd258acf4ef2cf72f
ReCodeRa <esosacek@centrum.cz>
2023-01-08 12:27:58 +0100

    With fileObject json closed

--- Key_iter.py
+++ Key_iter.py
@@ -2,7 +2,6 @@ import json
 fileObject = open("./MRI/ProductKeys.json", "r")
 jsonContent = fileObject.read()
 keys = json.loads(jsonContent)
-print(type(keys))
-# print(keys)
+fileObject.close()
 lsKeys = [d['ProductKey'] for d in keys]
 print(lsKeys[0:3])
