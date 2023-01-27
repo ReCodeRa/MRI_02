5f71e19561c852e2b4c231b4c2bceccde4647c89
ReCodeRa <esosacek@centrum.cz>
2023-01-27 21:31:05 +0100

    FUNCTIONAL for download of xlsx and merge

--- MRI/pw_down_merge.py
+++ MRI/pw_down_merge.py
@@ -32,7 +32,7 @@ import glob
 from os import getcwd
 print(getcwd())
 # WRONG!!!
-xl_path = f"{getcwd()}/MRI/res"
+xl_path = f"{getcwd()}/res"
 file_list = glob.glob(xl_path + "/*/*.xlsx")
 print(file_list)
 
@@ -43,7 +43,4 @@ for xl_file in file_list:
       xl_red, ignore_index=True)
  
 xl_merged.to_excel('res.xlsx', index=False)
-print(f'Resulting file written to: {getcwd()}')
-
-/home/recodera/MRI_dasa/MRI_02/res/NL_H_4002_001/NL_H_4002_001.xlsx
-    
+print(f'Resulting file written to: {getcwd()}')    
