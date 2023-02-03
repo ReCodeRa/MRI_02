d9c297c4c5865598d1ad6aba2a12ca5efd9cca24
ReCodeRa <esosacek@centrum.cz>
2023-02-03 11:40:15 +0100

    pdf not read

--- pdf.py
+++ pdf.py
@@ -1,7 +1,15 @@
 from tabula import read_pdf
 from tabulate import tabulate
 import os
+import camelot
  
-#reads table from pdf file
-df = read_pdf("home/recodera/MRI_dasa/MRI-02/Loper/Loper/Actavis Losiwuto PAR_2662 2014 likeLosidaru.pdf",pages="all") #address of pdf file
-print(tabulate(df))
+# #reads table from pdf file
+# print(os.getcwd())
+# df = read_pdf("~/MRI_dasa/MRI_02/Loper/Loper/Actavis_Losiwuto_PAR_2662_2014_likeLosidaru.pdf", pages="all") #address of pdf file
+# print(tabulate(df))
+
+print(os.getcwd())
+file = "home/MRI_dasa/MRI_02/Loper/Loper/Actavis_Losiwuto_PAR_2662_2014_likeLosidaru.pdf"
+ 
+tables = camelot.read_pdf(file, pages = "1-end")
+tables[0].df
