07072dc656504b2bb46fe447b0911dd9905a4342
ReCodeRa <esosacek@centrum.cz>
2023-02-03 12:49:33 +0100

    tabula not working yet

--- pdf.py
+++ pdf.py
@@ -1,17 +1,18 @@
-# from tabula import read_pdf
-# from tabulate import tabulate
+from tabula import read_pdf
+from tabulate import tabulate
 import os
-import camelot
- 
-# #reads table from pdf file
-# print(os.getcwd())
-# df = read_pdf("~/MRI_dasa/MRI_02/Loper/Loper/Actavis_Losiwuto_PAR_2662_2014_likeLosidaru.pdf", pages="all") #address of pdf file
-# print(tabulate(df))
+# import camelot
+
+file = "/home/recodera/MRI_dasa/MRI_02/Loper/Loper/LosiActa.pdf"
+
+#reads table from pdf file
+print(os.getcwd())
+df = read_pdf(file, pages="all") #address of pdf file
+print(tabulate(df))
 
 # print(os.getcwd())
 # pth = '/home/recodera/MRI_dasa/MRI_02/Loper/Loper/'
 # os.chdir(pth)
 # print(os.listdir())
-file = "/home/recodera/MRI_dasa/MRI_02/Loper/Loper/LosiActa.pdf"
- 
-tables = camelot.read_pdf(file)
+
+# tables = camelot.read_pdf(file)
