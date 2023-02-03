73472fbb8f975709a9c5d8171fa12580e8b40aee
ReCodeRa <esosacek@centrum.cz>
2023-02-03 12:33:02 +0100

    path ok camelot wroing pypdf version

--- pdf.py
+++ pdf.py
@@ -1,5 +1,5 @@
-from tabula import read_pdf
-from tabulate import tabulate
+# from tabula import read_pdf
+# from tabulate import tabulate
 import os
 import camelot
  
@@ -8,8 +8,10 @@ import camelot
 # df = read_pdf("~/MRI_dasa/MRI_02/Loper/Loper/Actavis_Losiwuto_PAR_2662_2014_likeLosidaru.pdf", pages="all") #address of pdf file
 # print(tabulate(df))
 
-print(os.getcwd())
-file = "home/MRI_dasa/MRI_02/Loper/Loper/Actavis_Losiwuto_PAR_2662_2014_likeLosidaru.pdf"
+# print(os.getcwd())
+# pth = '/home/recodera/MRI_dasa/MRI_02/Loper/Loper/'
+# os.chdir(pth)
+# print(os.listdir())
+file = "/home/recodera/MRI_dasa/MRI_02/Loper/Loper/LosiActa.pdf"
  
-tables = camelot.read_pdf(file, pages = "1-end")
-tables[0].df
+tables = camelot.read_pdf(file)
