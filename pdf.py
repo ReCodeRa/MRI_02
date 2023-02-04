bd42be79ca778d6a6f63878ec2339328457e33b8
ReCodeRa <esosacek@centrum.cz>
2023-02-04 21:30:36 +0100

    PK values in list :)

--- pdf.py
+++ pdf.py
@@ -7,8 +7,35 @@ file = "/home/recodera/MRI_dasa/MRI_02/Loper/Loper/LosiActa.pdf"
 
 #reads table from pdf file
 print(os.getcwd())
-df = read_pdf(file, pages="all") #address of pdf file
-print(tabulate(df))
+ls_df = read_pdf(file, pages="all") #address of pdf file
+# print(tabulate(ls_df))
+
+for tb in ls_df:
+    # tb.info
+    # print(list(tb.columns))
+    # tb.head()
+    # print(tb.between(0, 2, inclusive=True))
+    # df_new = df.iloc[:, 0:3]
+    df_str = tb.to_string()
+    print(df_str)
+
+
+df_str = ls_df[0].to_string()
+print(df_str)
+
+ls_df[0].to_csv('df_csv.csv')
+
+## RegEx
+import re
+r = re.compile(r"[01][.]{1}\d{2}")
+with open('df_csv.csv') as f:
+    s = f.read()
+
+dec = re.findall(r, s)
+print(dec)
+
+
+dec = 
 
 # print(os.getcwd())
 # pth = '/home/recodera/MRI_dasa/MRI_02/Loper/Loper/'
