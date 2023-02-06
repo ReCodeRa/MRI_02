c24ee0f8c3a925eaf78c68d06c6f677f064b02b4
ReCodeRa <esosacek@centrum.cz>
2023-02-06 13:19:12 +0100

    id to folder

--- pdf_f.py
+++ pdf_f.py
@@ -6,7 +6,11 @@ def pdf_tab_id(file):
     import os
     import re
     import json
+    import pathlib
     
+    fce_path_file = pathlib.Path(file)
+    fce_path = fce_path_file.parent
+    print(f'fce_path is: {fce_path}')
     ls_df = read_pdf(file, pages="all")
     r = re.compile(r"[01][.]{1}\d{2}")
     ls_id = []
@@ -18,8 +22,10 @@ def pdf_tab_id(file):
         ls_id.append(dec)
 
     js_str = json.dumps(ls_id)
-    jsonFile = open("id.json", "w")
+    jsonFile = open(f"{fce_path}/id.json", "w")
     jsonFile.write(js_str)
     jsonFile.close()
 
 pdf_tab_id(file)
+
+pathlib.Path.
