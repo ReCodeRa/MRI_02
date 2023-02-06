30fb27d00071d4b1c3b68387b3e3daedd2744938
ReCodeRa <esosacek@centrum.cz>
2023-02-06 12:41:08 +0100

    id.json functional :) Next specify folder

--- pdf_f.py
+++ pdf_f.py
@@ -9,14 +9,17 @@ def pdf_tab_id(file):
     
     ls_df = read_pdf(file, pages="all")
     r = re.compile(r"[01][.]{1}\d{2}")
+    ls_id = []
     for count, tb in enumerate(ls_df):
         ls_df[count].to_csv('df_csv.csv')
         with open('df_csv.csv') as f:
             s = f.read()
         dec = re.findall(r, s)
-        js_str = json.dumps(dec)
-        jsonFile = open("id.json", "w")
-        jsonFile.write(js_str)
-        jsonFile.close()
+        ls_id.append(dec)
+
+    js_str = json.dumps(ls_id)
+    jsonFile = open("id.json", "w")
+    jsonFile.write(js_str)
+    jsonFile.close()
 
 pdf_tab_id(file)
